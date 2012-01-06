3beb90e02a8bcea561de05d30faf402a1d9d2292
Zdenek Styblik <stybla@turnovfree.net>
2012-01-06 07:24:32 +0100

    MOD build script for kernel Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    Adds auto-download of kernel source in case tarball is not found.

--- k/kernel-huge/build.sh
+++ k/kernel-huge/build.sh
@@ -9,10 +9,23 @@ NUMJOBS=${NUMJOBS:-"-j7"}
 
 if [ ! -e $CWD/linux-${KERNNAME}-${KVERSION}.config ]; then
 	echo "Kernel config not found."
-	exit 1;
+	exit 1
 fi
 
 cd /usr/src/
+if [ ! -e "linux-${KVERSION}.tar.bz2" ]; then
+	KVERPART=$(printf "%s" "${KVERSION}" | cut -d '.' -f 1-2)
+	if [ -z "${KVERPART}" ] || ! printf "%s" "${KVERPART}" | grep -e '[0-9]\.[0-9]'; then
+		printf "KVERSION '%s' is garbage. Expected X.Y.Z...\n" "${KVERSION}"
+		exit 1
+	fi # if [ -z "${VERPART}" ]
+	if printf "%s" "${KVERPART}" | grep -q -e '3\.[0-9]' ; then
+		# Note: Let's see when this is going to break
+		KVERPART="3.0"
+	fi
+	wget \
+		"http://www.kernel.org/pub/linux/kernel/v${KVERPART}/linux-${KVERSION}.tar.bz2"
+fi # if [ ! -e "linux-${KVERSION}.tar.bz2" ]
 tar vjxf $CWD/linux-${KVERSION}.tar.bz2 || exit 1
 cd linux-${KVERSION} || exit 2
 make clean
